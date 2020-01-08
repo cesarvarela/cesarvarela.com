13fdcb2e382065cf67d7e958745e0ac1a45a92c3
Cesar Varela <mail@cesarvarela.com>
2020-01-07 21:41:41 -0300

    Update Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,10 +7,6 @@ export function Header() {
 
 	return <Section id="head" title="Cesar Varela">
 		<img className="po" src={picture} alt="po face" />
-		<p className="title">Frontend Engineer &amp; UX/UI Consultant</p>
-		<p className="po-quote">
-			Hi, my name is Cesar Varela, this is my personal website,
-			scroll down to know a little more about me.
-					</p>
+		<p className="quote">Mostly self-thought, I've been a <b>Software Developer &amp; Consultant</b> for more than 15 years, always with an eye on both Design and User Experience.</p>
 	</Section>
 }
