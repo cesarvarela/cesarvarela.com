6271daf8a459f7c3e7356fe4672fda6c4494e95f
Cesar Varela <mail@cesarvarela.com>
2020-01-09 16:39:03 -0300

    Use styled components for Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,40 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
+import styled from 'styled-components'
+
+const StyledCard = styled.div`
+    border: 1px solid #c4c4c4;
+    text-align: left;
+    & > .image {
+        background-size: cover;
+        padding-bottom: 50%;
+        background-position: center;
+        border-bottom: 1px solid #c4c4c4;
+    }
+    & > h3 {
+        padding: 12px;
+        margin: 0;
+    }
+    & > p {
+        padding: 12px;
+        margin: 0;
+    }
+    & > a {
+        padding: 12px;
+        margin: 0;
+        text-decoration: none;
+        display: block;
+        color: $text-dark;
+    }
+`
 
 export default function Card({ src, title, description, href }) {
 
-    return <div className="card">
+    return <StyledCard>
         <div className="image" style={{ backgroundImage: `url(${src})` }} />
         <h3>{title}</h3>
         <p>{description}</p>
         <a href={href} target="_blank">View more <FontAwesomeIcon icon={faExternalLinkAlt} /></a>
-    </div>
+    </StyledCard>
 }
