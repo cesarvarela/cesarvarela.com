74edf1e2867dc8819434aae36aface02568f87f4
Cesar Varela <mail@cesarvarela.com>
2020-01-14 19:47:52 -0300

    Update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,36 @@
 import React from "react"
-
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
+import styled from "styled-components"
+import { Link } from "gatsby"
+import mario from '../images/404.gif'
+
+
+const NotFound = styled.div`
+  margin: 12px auto 0;
+  min-height: 100vh;
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  background-image: url(${mario});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  color: #efefef;
+
+  & > p a {
+    color: #efefef;
+  }
+`
 
 const NotFoundPage = () => (
-    <Layout>
-      <SEO title="404: Not found" />
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Layout>
+  <Layout>
+    <SEO title="404: Not found" />
+    <NotFound>
+      <p>Not sure how you got here but you should probably <Link to={"/"}>go home</Link></p>
+    </NotFound>
+  </Layout>
 )
 
 export default NotFoundPage
