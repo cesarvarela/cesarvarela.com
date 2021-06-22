4a762171e3b0e8809d46e9f17e4c8c221ab07ed6
Cesar Varela <mail@cesarvarela.com>
2021-06-22 19:42:44 -0300

    Add grommet icons

--- src/lib/theme.js
+++ src/lib/theme.js
@@ -4,6 +4,22 @@ export const lightTheme = {
     color: '#333',
     boxShadow: '4px 6px 25px rgba(0, 0, 0, 0.24)',
     border: '#E0E0E0',
+
+    //for grommet
+    global: {
+        colors: {
+            icon: '#333',
+        }
+    },
+    icon: {
+        size: {
+            small: '12px',
+            medium: '24px',
+            large: '48px',
+            xlarge: '96px',
+        },
+        extend: undefined,
+    },
 }
 
 export const darkTheme = {
@@ -12,10 +28,42 @@ export const darkTheme = {
     color: '#f1f1f1',
     boxShadow: '0px 4px 29px 15px rgba(255, 255, 255, 0.18)',
     border: '#525252',
+
+    //for grommet
+    global: {
+        colors: {
+            icon: '#f1f1f1',
+        }
+    },
+    icon: {
+        size: {
+            small: '12px',
+            medium: '24px',
+            large: '48px',
+            xlarge: '96px',
+        },
+        extend: undefined,
+    },
 }
 
 export const ssrTheme = {
     name: 'ssr',
     background: 'inherit',
     color: 'inherit',
+
+    //for grommet
+    global: {
+        colors: {
+            icon: '#666666',
+        }
+    },
+    icon: {
+        size: {
+            small: '12px',
+            medium: '24px',
+            large: '48px',
+            xlarge: '96px',
+        },
+        extend: undefined,
+    },
 }
