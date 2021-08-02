196f8ea41c293980ed41fc394aa329d754bd6127
Cesar Varela <mail@cesarvarela.com>
2021-08-01 21:26:47 -0300

    Move svg to their own folder

--- src/lib/theme.js
+++ src/lib/theme.js
@@ -1,69 +1,24 @@
-export const lightTheme = {
-    name: 'light',
-    background: '#fff',
-    color: '#333',
-    boxShadow: '4px 6px 25px rgba(0, 0, 0, 0.24)',
-    border: '#E0E0E0',
-
-    //for grommet
-    global: {
-        colors: {
-            icon: '#333',
-        }
-    },
-    icon: {
-        size: {
-            small: '12px',
-            medium: '24px',
-            large: '48px',
-            xlarge: '96px',
+const theme = {
+    mode: 'light',
+    colors: {
+        background: {
+            light: '#fff',
+            dark: '#000',
         },
-        extend: undefined,
+        foreground: {
+            light: '#000',
+            dark: '#fff',
+        },
     },
+    card: {
+        boxShadow: {
+            light: '4px 6px 25px rgba(0, 0, 0, 0.24)',
+            dark: '0px 4px 29px 15px rgba(255, 255, 255, 0.18)',
+        },
+    }
 }
 
-export const darkTheme = {
-    name: 'dark',
-    background: '#000',
-    color: '#f1f1f1',
-    boxShadow: '0px 4px 29px 15px rgba(255, 255, 255, 0.18)',
-    border: '#525252',
+export const getColor = (name) => ({ theme }) => theme.colors[name][theme.mode]
+export const getProp = (name, prop) => ({ theme }) => theme[name][prop][theme.mode]
 
-    //for grommet
-    global: {
-        colors: {
-            icon: '#f1f1f1',
-        }
-    },
-    icon: {
-        size: {
-            small: '12px',
-            medium: '24px',
-            large: '48px',
-            xlarge: '96px',
-        },
-        extend: undefined,
-    },
-}
-
-export const ssrTheme = {
-    name: 'ssr',
-    background: 'inherit',
-    color: 'inherit',
-
-    //for grommet
-    global: {
-        colors: {
-            icon: '#666666',
-        }
-    },
-    icon: {
-        size: {
-            small: '12px',
-            medium: '24px',
-            large: '48px',
-            xlarge: '96px',
-        },
-        extend: undefined,
-    },
-}
+export default theme
